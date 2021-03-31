5075fed2015f2ee53b62063966010d08b501b1e5
gustavo.silva <gustavo.silva@seplag.ce.gov.br>
2021-03-31 14:41:22 -0300

    Atividade Concluida

---
+++ layouts/parts/singles/agent-form.php
@@ -0,0 +1,150 @@
+<div class="ficha-spcultura">
+    <?php if($this->isEditable() && $entity->shortDescription && strlen($entity->shortDescription) > 2000): ?>
+        <div class="alert warning">
+            <?php \MapasCulturais\i::_e("O limite de caracteres da descrição curta foi diminuido para 400, mas seu texto atual possui");?>
+            <?php echo strlen($entity->shortDescription) ?>
+            <?php \MapasCulturais\i::_e("caracteres. Você deve alterar seu texto ou este será cortado ao salvar.");?>
+        </div>
+    <?php endif; ?>      
+    
+    <div class="alert warning"> 
+        <span class="label">Descrição Curta</span><span class="label" style="color: Red" >(CAMPO OBRIGATÓRIO):</span>   
+        <span class="js-editable <?php echo ($entity->isPropertyRequired($entity,"shortDescription") && $editEntity? 'label': '');?>" data-edit="shortDescription" data-original-title="<?php \MapasCulturais\i::esc_attr_e("Descrição Curta");?>" data-emptytext="<?php \MapasCulturais\i::esc_attr_e("Insira uma Descrição Curta");?>" data-showButtons="bottom" data-tpl='<textarea maxlength="400"></textarea>'><?php echo $this->isEditable() ? $entity->shortDescription : nl2br($entity->shortDescription); ?></span>
+    </div>
+
+<!-- Widget-area Inicio -->
+<div class="alert warning">
+    <?php
+        $entityClass = $entity->getClassName();
+        $entityName = strtolower(array_slice(explode('\\', $entityClass),-1)[0]);
+        $areas = array_values($app->getRegisteredTaxonomy($entityClass, 'area')->restrictedTerms);
+        sort($areas);
+    ?>
+        <div class="widget">
+            <h3> <span class="label"></span> <?php $this->dict('taxonomies:area: name') ?><span class="label" style="color: Red">(CAMPO OBRIGATÓRIO):</span></h3>
+            <?php if($this->isEditable()): ?>
+                <span id="term-area" class="js-editable-taxonomy" data-original-title="<?php $this->dict('taxonomies:area: name') ?>" data-emptytext="<?php $this->dict('taxonomies:area: select at least one') ?>" data-restrict="true" data-taxonomy="area"><?php echo implode('; ', $entity->terms['area'])?></span>
+            <?php else: ?>
+                <?php
+                foreach($areas as $i => $t): if(in_array($t, $entity->terms['area'])): ?>
+                    <a class="tag tag-<?php echo $this->controller->id ?>" href="<?php echo $app->createUrl('site', 'search') ?>##(<?php echo $entityName ?>:(areas:!(<?php echo $i ?>)),global:(enabled:(<?php echo $entityName ?>:!t),filterEntity:<?php echo $entityName ?>))">
+                        <?php echo $t ?>
+                    </a>
+                <?php endif; endforeach; ?>
+            <?php endif;?>
+        </div>
+        </div>
+      
+<!-- Widget-area Fim -->
+
+    <?php $this->applyTemplateHook('tab-about-service','before'); ?><!--. hook tab-about-service:before -->
+
+    <div class="servico">
+        <?php $this->applyTemplateHook('tab-about-service','begin'); ?><!--. hook tab-about-service:begin -->
+
+        <?php if($this->isEditable() || $entity->site): ?>
+            <p><span class="label"><?php \MapasCulturais\i::_e("Site");?>:</span>
+            <?php if($this->isEditable()): ?>
+                <span class="js-editable <?php echo ($entity->isPropertyRequired($entity,"site") && $editEntity? 'required': '');?>" data-edit="site" data-original-title="<?php \MapasCulturais\i::esc_attr_e("Site");?>" data-emptytext="<?php \MapasCulturais\i::esc_attr_e("Insira a url de seu site");?>"><?php echo $entity->site; ?></span></p>
+            <?php else: ?>
+                <a class="url" href="<?php echo $entity->site; ?>"><?php echo $entity->site; ?></a>
+            <?php endif; ?>
+        <?php endif; ?>
+
+        <?php if($this->isEditable()): ?>
+            <!-- Campo Nome Completo -->
+            <p class="privado">
+                <span class="icon icon-private-info"></span>
+                <span class="label"><?php \MapasCulturais\i::_e("Nome Completo");?>:</span>
+                <span class="js-editable <?php echo ($entity->isPropertyRequired($entity,"nomeCompleto") && $editEntity? 'required': '');?>" data-edit="nomeCompleto" data-original-title="<?php \MapasCulturais\i::esc_attr_e("Nome Completo ou Razão Social");?>" data-emptytext="<?php \MapasCulturais\i::esc_attr_e("Informe seu nome completo ou razão social");?>">
+                    <?php echo $entity->nomeCompleto; ?>
+                </span>
+            </p>
+            <!-- Campo CPF/CNPJ -->
+            <p class="privado">
+                <span class="icon icon-private-info"></span>
+                <span class="label"><?php \MapasCulturais\i::_e("CPF/CNPJ");?>:</span>
+                <span class="js-editable <?php echo ($entity->isPropertyRequired($entity,"documento") && $editEntity? 'required': '');?>" data-edit="documento" data-original-title="<?php \MapasCulturais\i::esc_attr_e("CPF/CNPJ");?>" data-emptytext="<?php \MapasCulturais\i::esc_attr_e("Informe seu CPF ou CNPJ com pontos, hífens e barras");?>">
+                    <?php echo $entity->documento; ?>
+                </span>
+            </p>
+            <!-- Campo Data de Nascimento / Fundação -->
+            <p class="privado">
+                <span class="icon icon-private-info"></span>
+                <span class="label"><?php \MapasCulturais\i::_e("Data de Nascimento/Fundação");?>:</span>
+                <span class="js-editable <?php echo ($entity->isPropertyRequired($entity,"dataDeNascimento") && $this->isEditable()? 'required': '');?>" <?php echo $entity->dataDeNascimento ? "data-value='".$entity->dataDeNascimento . "'" : ''?>  data-type="date" data-edit="dataDeNascimento" data-viewformat="dd/mm/yyyy" data-showbuttons="false" data-original-title="<?php \MapasCulturais\i::esc_attr_e("Data de Nascimento/Fundação");?>" data-emptytext="<?php \MapasCulturais\i::esc_attr_e("Insira a data de nascimento ou fundação do agente");?>">
+                    <?php $dtN = (new DateTime)->createFromFormat('Y-m-d', $entity->dataDeNascimento); echo $dtN ? $dtN->format('d/m/Y') : ''; ?>
+                </span>
+            </p>
+            <!-- Gênero -->
+            <p class="privado">
+                <span class="icon icon-private-info"></span>
+                <span class="label"><?php \MapasCulturais\i::_e("Gênero");?>:</span>
+                <span class="js-editable <?php echo ($entity->isPropertyRequired($entity,"genero") && $editEntity? 'required': '');?>" data-edit="genero" data-original-title="<?php \MapasCulturais\i::esc_attr_e("Gênero");?>" data-emptytext="<?php \MapasCulturais\i::esc_attr_e("Selecione o gênero se for pessoa física");?>">
+                    <?php echo $entity->genero; ?>
+                </span>
+            </p>
+            <!-- Orientação Sexual -->
+            <p class="privado"><span class="icon icon-private-info"></span>
+                <span class="label"><?php \MapasCulturais\i::_e("Orientação Sexual");?>:</span>
+                <span class="js-editable <?php echo ($entity->isPropertyRequired($entity,"orientacaoSexual") && $editEntity? 'required': '');?>" data-edit="orientacaoSexual" data-original-title="<?php \MapasCulturais\i::esc_attr_e("Orientação Sexual");?>" data-emptytext="<?php \MapasCulturais\i::esc_attr_e("Selecione uma orientação sexual se for pessoa física");?>">
+                    <?php echo $entity->orientacaoSexual; ?>
+                </span>
+            </p>
+            <!-- Raça/Cor -->
+            <p class="privado"><span class="icon icon-private-info"></span>
+                <span class="label"><?php \MapasCulturais\i::_e("Raça/Cor");?>:</span>
+                <span class="js-editable  <?php echo ($entity->isPropertyRequired($entity,"raca") && $editEntity? 'required': '');?>" data-edit="raca" data-original-title="<?php \MapasCulturais\i::esc_attr_e("Raça/cor");?>" data-emptytext="<?php \MapasCulturais\i::esc_attr_e("Selecione a raça/cor se for pessoa física");?>">
+                    <?php echo $entity->raca; ?>
+                </span>
+            </p>
+            <!-- E-mail privado-->
+            <p class="privado"><span class="icon icon-private-info"></span>
+                <span class="label"><?php \MapasCulturais\i::_e("Email Privado");?>:</span>
+                <span class="js-editable <?php echo ($entity->isPropertyRequired($entity,"emailPrivado") && $editEntity? 'required': '');?>" data-edit="emailPrivado" data-original-title="<?php \MapasCulturais\i::esc_attr_e("Email Privado");?>" data-emptytext="<?php \MapasCulturais\i::esc_attr_e("Insira um email que não será exibido publicamente");?>">
+                    <?php echo $entity->emailPrivado; ?>
+                </span>
+            </p>
+        <?php endif; ?>
+
+        <!-- Email Público -->
+        <?php if($this->isEditable() || $entity->emailPublico): ?>
+            <p><span class="label"><?php \MapasCulturais\i::_e("E-mail");?>:</span>
+                <span class="js-editable <?php echo ($entity->isPropertyRequired($entity,"emailPublico") && $this->isEditable()? 'required': '');?>" data-edit="emailPublico" data-original-title="<?php \MapasCulturais\i::esc_attr_e("Email Público");?>" data-emptytext="<?php \MapasCulturais\i::esc_attr_e("Insira um email que será exibido publicamente");?>">
+                    <?php echo $entity->emailPublico; ?>
+                </span>
+            </p>
+        <?php endif; ?>
+
+        <!-- Email Público -->
+        <?php if($this->isEditable() || $entity->telefonePublico): ?>
+            <p><span class="label"><?php \MapasCulturais\i::_e("Telefone Público");?>:</span>
+                <span class="js-editable js-mask-phone <?php echo ($entity->isPropertyRequired($entity,"telefonePublico") && $this->isEditable()? 'required': '');?>" data-edit="telefonePublico" data-original-title="<?php \MapasCulturais\i::esc_attr_e("Telefone Público");?>" data-emptytext="<?php \MapasCulturais\i::esc_attr_e("Insira um telefone que será exibido publicamente");?>">
+                    <?php echo $entity->telefonePublico; ?>
+                </span>
+            </p>
+        <?php endif; ?>
+
+        <?php if($this->isEditable()): ?>
+            <!-- Telefone Privado 1 -->
+            <p class="privado"><span class="icon icon-private-info"></span>
+                <span class="label"><?php \MapasCulturais\i::_e("Telefone 1");?>:</span>
+                <span class="js-editable js-mask-phone <?php echo ($entity->isPropertyRequired($entity,"telefone1") && $this->isEditable()? 'required': '');?>" data-edit="telefone1" data-original-title="<?php \MapasCulturais\i::esc_attr_e("Telefone Privado");?>" data-emptytext="<?php \MapasCulturais\i::esc_attr_e("Insira um telefone que não será exibido publicamente");?>">
+                    <?php echo $entity->telefone1; ?>
+                </span>
+            </p>
+            <!-- Telefone Privado 2 -->
+            <p class="privado"><span class="icon icon-private-info"></span>
+                <span class="label"><?php \MapasCulturais\i::_e("Telefone 2");?>:</span>
+                <span class="js-editable js-mask-phone <?php echo ($entity->isPropertyRequired($entity,"telefone2") && $this->isEditable()? 'required': '');?>" data-edit="telefone2" data-original-title="<?php \MapasCulturais\i::esc_attr_e("Telefone Privado");?>" data-emptytext="<?php \MapasCulturais\i::esc_attr_e("Insira um telefone que não será exibido publicamente");?>">
+                    <?php echo $entity->telefone2; ?>
+                </span>
+            </p>
+        <?php endif; ?>
+
+        <?php $this->applyTemplateHook('tab-about-service','end'); ?><!--. hook tab-about-service:end -->
+    </div><!--.servico -->
+
+    <?php $this->applyTemplateHook('tab-about-service','after'); ?><!--. hook tab-about-service:after -->
+
+</div>
